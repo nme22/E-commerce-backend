29f4a6125255e08a4adcb2f6023db784c4db2f43
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-04-15 15:37:59 -0400

    got the category routes to work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,54 +7,41 @@ const {
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-
   Category.findAll({
-    include: {
-      model: Product,
-      attribute: ["id", "product_name", "price", "stock", "category_id"]
-    }
-  })
+      include: [Product],
+    })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: {
-      model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"]
-    }
-  })
+      where: {
+        id: req.params.id,
+      },
+      include: [Product],
+    })
+    .then((category) => res.json(category))
 });
 
 router.post('/', (req, res) => {
-  // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
+  Category.create(req.body).then((category) => res.status(200).json(category))
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update(req.body, {
     where: {
-      id: req.params.id
-    }
-  })
+      id: req.params.id,
+    },
+  }).then((category) => res.status(200).json(category))
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where: {
-      id: req.params.id
-    }
-  })
+      id: req.params.id,
+    },
+  }).then((category) => res.status(200).json(category))
 });
 
 module.exports = router;
