9926beec3199dadffbc815a2302a9a65067cbfa2
Navid Ebahimi <navidmebrahimi22@gmail.com>
2021-04-15 16:09:30 -0400

    almost done...

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,15 +10,34 @@ const {
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+      include: [{
+        model: Product,
+        through: ProductTag,
+      }, ],
+    })
+    .then((tags) => res.status(200).json(tags))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [{
+        model: Product,
+        through: ProductTag,
+      }, ],
+    })
+    .then((tag) => res.status(200).json(tag))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
 });
 
 router.put('/:id', (req, res) => {
